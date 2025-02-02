d2d8ca0337b7f5c462a3a0c7a0b92726050452a2
yadaiio <y.clintjens@gmail.com>
2025-02-02 15:36:57 +0100

    add TicYaMo fields

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args){
+
+        //String für das Feld, Schachbrett, regulär leer
+        String a1 = " ";
+        String a2 = " ";
+        String a3 = " "; 
+        String b1 = " "; 
+        String b2 = " "; 
+        String b3 = " "; 
+        String c1 = " "; 
+        String c2 = " "; 
+        String c3 = " ";
+    }
+}
