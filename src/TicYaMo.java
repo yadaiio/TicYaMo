81eb34001e0512f86deab58869779fd99693e25c
yadaiio <y.clintjens@gmail.com>
2025-02-02 15:45:06 +0100

    new name for class TicYaMo and make all Strings public

---
+++ src/TicYaMo.java
@@ -0,0 +1,13 @@
+public class TicYaMo {
+
+    //String für das Feld, Schachbrett, regulär leer
+    public String a1 = " ";
+    public String a2 = " ";
+    public String a3 = " "; 
+    public String b1 = " "; 
+    public String b2 = " "; 
+    public String b3 = " "; 
+    public String c1 = " "; 
+    public String c2 = " "; 
+    public String c3 = " ";
+}
