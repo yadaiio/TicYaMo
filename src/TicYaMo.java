3e8f8021daca39a10e792bcfbf89a3b4d4bf94b6
yadaiio <y.clintjens@gmail.com>
2025-02-09 15:50:35 +0100

    50 line breaks to clear the cmd

--- src/TicYaMo.java
+++ src/TicYaMo.java
@@ -12,9 +12,18 @@ public class TicYaMo {
     public String c2 = " "; 
     public String c3 = " ";
 
+    //Der eigentliche Spieler
+    public String player = "x";
+
     //Methode zum Spiel zeichnen
     public void zeichneSpiel() {
-        System.out.println("   | A | B | C |");
+
+        //Für die 50 Zeilenumbrüch, da Java nicht clearen kann
+        for(int i = 0;i > 50;i++) {
+            System.out.println(" ");
+        }
+
+        System.out.println( " " + player + " | A | B | C |");
         System.out.println("---+---+---+---┤");
         System.out.println(" 1 | " + a1 + " | " + b1 + " | " + c1 + " |");
         System.out.println("---+---+---+---┤");
