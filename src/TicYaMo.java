527d40e2dd317ca8961d2587ef319789015130bf
Yada Clintjens <y.clintjens@gmail.com>
2025-02-14 17:46:17 +0100

    make attributes private and shorten the comparison with .toLowerCase()

--- src/TicYaMo.java
+++ src/TicYaMo.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 //Ansammlung von den Funktionen, die TicYAMo generell können sollte.
 public class TicYaMo {
 
-    //String für das Feld, Schachbrett, regulär leer, für den Aufruf in main müssen sie public sein
-    public String a1 = " ";
-    public String a2 = " ";
-    public String a3 = " "; 
-    public String b1 = " "; 
-    public String b2 = " "; 
-    public String b3 = " "; 
-    public String c1 = " "; 
-    public String c2 = " "; 
-    public String c3 = " ";
+    //String für das Feld, Schachbrett, regulär leer
+    private String a1 = " ";
+    private String a2 = " ";
+    private String a3 = " "; 
+    private String b1 = " "; 
+    private String b2 = " "; 
+    private String b3 = " "; 
+    private String c1 = " "; 
+    private String c2 = " "; 
+    private String c3 = " ";
 
     //Der eigentliche Spieler
-    public String player = "x";
+    private String player = "x";
 
     tools tool = new tools();
     Scanner nextTurnScanner = new Scanner(System.in);
@@ -41,59 +41,51 @@ public class TicYaMo {
         String spielzug = nextTurnScanner.nextLine();
 
         //Der Vergleich ob das Feld leer ist und ob die Eingabe den Namen des Feldes gleicht
-        if(spielzug.equals("a1") && a1.equals(" ") || spielzug.equals("A1") && a1.equals(" ")) {
+        if(spielzug.toLowerCase().equals("a1") && a1.equals(" ")) {
             a1 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("a2") && a2.equals(" ") || spielzug.equals("A2") && a2.equals(" ")) {
+        if(spielzug.toLowerCase().equals("a2") && a2.equals(" ")) {
             a2 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("a3") && a3.equals(" ") || spielzug.equals("A3") && a3.equals(" ")) {
+        if(spielzug.toLowerCase().equals("a3") && a3.equals(" ")) {
             a3 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("b1") && b1.equals(" ") || spielzug.equals("B1") && b1.equals(" ")) {
+        if(spielzug.toLowerCase().equals("b1") && b1.equals(" ")) {
             b1 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("b2") && b2.equals(" ") || spielzug.equals("B2") && b2.equals(" ")) {
+        if(spielzug.toLowerCase().equals("b2") && b2.equals(" ")) {
             b2 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("b3") && b3.equals(" ") || spielzug.equals("B3") && b3.equals(" ")) {
+        if(spielzug.toLowerCase().equals("b3") && b3.equals(" ")) {
             b3 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("c1") && c1.equals(" ") || spielzug.equals("C1") && c1.equals(" ")) {
+        if(spielzug.toLowerCase().equals("c1") && c1.equals(" ")) {
             c1 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("c2") && c2.equals(" ") || spielzug.equals("C2") && c2.equals(" ")) {
+        if(spielzug.toLowerCase().equals("c2") && c2.equals(" ")) {
             c2 = player;
-            changePlayer();
             
         }
 
-        if(spielzug.equals("c3") && c3.equals(" ") || spielzug.equals("C3") && c3.equals(" ")) {
+        if(spielzug.toLowerCase().equals("c3") && c3.equals(" ")) {
             c3 = player;
-            changePlayer();
             
         }
+        changePlayer();
     }
 
     public void changePlayer() {
